9f3439023a911b10c45f5e69bd0213bde5ef111f
shaik.aqib1 <shaik.aqib@wipro.com>
2020-06-13 00:01:41 +0100

    changes

---
+++ src/main/java/com/practice/clientapplication/ClientapplicationApplication.java
@@ -0,0 +1,13 @@
+package com.practice.clientapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/clientapplication/ClientapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.clientapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
